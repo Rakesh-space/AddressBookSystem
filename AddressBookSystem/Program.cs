1a6cd627f8fe7d2551be43ee83236083e132465d
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-28 21:59:45 +0530

    Addint UC1 Address Book System.

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,7 +6,18 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n Welcome to Hello World!");
+            ContactList contact = new ContactList();
+            contact.firstName = "Rakesh";
+            contact.lastName = "Musale";
+            contact.phoneNumber = "8007078569";
+            contact.address = "Pune";
+            contact.state = "Maharashtra";
+            contact.email = "rakeshmusale111@gmail.com";
+            contact.city = "Pune";
+            contact.zip = "411013";
+
+            Console.WriteLine("\n First Name: {0} \n Last Name: {1} \n Address: {2} \n Phone Number: {3}\n Email: {4} \n City: {5} \n State: {6}\n ZipCode: {7}", contact.firstName, contact.lastName, contact.address, contact.phoneNumber, contact.email, contact.city, contact.state, contact.zip);
+            Console.ReadLine();
         }
     }
 }
