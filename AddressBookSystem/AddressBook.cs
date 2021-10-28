dc8cdf949bd48948869686e632a422f160ec84da
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-28 22:25:09 +0530

    Adding UC3 Address Book System

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -42,20 +42,63 @@ namespace AddressBookSystem
         }
         public void View()
         {
-            Console.WriteLine("This is your contact details :");
-
-            foreach (var Detailing in contacts)
+            if (contacts.Count == 0)  // this if statement shows that there is nothing in the list
             {
-
-                Console.WriteLine("first name = " + Detailing.firstName);
-                Console.WriteLine("last name = " + Detailing.lastName);
-                Console.WriteLine("address = " + Detailing.address);
-                Console.WriteLine("state = " + Detailing.state);
-                Console.WriteLine("city = " + Detailing.city);
-                Console.WriteLine("zip no = " + Detailing.zip);
-                Console.WriteLine("phone number = " + Detailing.phoneNumber);
-                Console.WriteLine("email ID = " + Detailing.email);
+                Console.WriteLine("Currently there are no people added in your addressbook.");
             }
+            else
+            {
+                Console.WriteLine("Here is the list and details of all the contacts in your addressbook.");
+
+                foreach (var Detailing in contacts)  //this foreacch loops iterates through all the contacts objects in the contacts class
+                {
+
+                    Console.WriteLine("first name = " + Detailing.firstName);
+                    Console.WriteLine("last name = " + Detailing.lastName);
+                    Console.WriteLine("address = " + Detailing.address);
+                    Console.WriteLine("state = " + Detailing.state);
+                    Console.WriteLine("city = " + Detailing.city);
+                    Console.WriteLine("zip no = " + Detailing.zip);
+                    Console.WriteLine("phone number = " + Detailing.phoneNumber);
+                    Console.WriteLine("email ID = " + Detailing.email);
+                }
+            }
+        }
+        public void Edit()  //this method lets the user edit the details based on their firstname
+        {
+            Console.WriteLine("Enter the first name of the contact you want to Modify.");
+            Console.WriteLine();
+            string fName = Console.ReadLine(); // taking the input of first name
+            foreach (var Details in contacts)
+            {
+                if (fName == Details.firstName)
+                {
+                    // below codes are similar to that of adding a contact.
+                    Console.Write("Enter First Name - ");
+                    Details.firstName = Console.ReadLine();
+                    Console.Write("Enter Last Name - ");
+                    Details.lastName = Console.ReadLine();
+                    Console.Write("Enter Address - ");
+                    Details.address = Console.ReadLine();
+                    Console.Write("Enter Phone number - ");
+                    Details.phoneNumber = Console.ReadLine();
+                    Console.Write("Enter Email ID - ");
+                    Details.email = Console.ReadLine();
+                    Console.Write("Enter City - ");
+                    Details.city = Console.ReadLine();
+                    Console.Write("Enter State - ");
+                    Details.state = Console.ReadLine();
+                    Console.Write("Enter ZIP code - ");
+                    Details.zip = Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No such entry found. Please check and try again!");
+                    break;
+                }
+            }
+
         }
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,28 +7,35 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             int num;
+            string keyPress = "o";
             //guide to user 
             Console.WriteLine("_Welcome to the address book program_");
-            Console.WriteLine();
             Console.WriteLine("Select the option.");
-            Console.WriteLine("1- Add contact, 2- View contact");
-            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1- Add contact, 2- View contact,3-edit contact");
+
             Console.WriteLine();
-
-            switch (num)               //switch case 
+            AddressBook AddObj = new AddressBook();
+            num = Convert.ToInt32(Console.ReadLine());
+            while (keyPress != "n")
             {
-                case 1:
-                    AddressBook AddObj = new AddressBook();
-                    AddObj.AddContact();
-                    AddObj.View();
-                    break;
+                switch (num)               //switch case 
+                {
+                    case 1:
+                        AddObj.AddContact();
+                        break;
 
-                case 2:
-                    AddressBook Addobj2 = new AddressBook();
-                    Addobj2.View();
-                    break;
+                    case 2:
+                        AddObj.View();
+                        break;
+
+                    case 3:
+                        AddObj.Edit();
+                        break;
+                }
+                Console.WriteLine("Do you wish to continue? Press (y/n)");
+                keyPress = Console.ReadLine();
             }
-            Console.ReadLine();
+        Console.ReadLine();
         }
     } 
 }
